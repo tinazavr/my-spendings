44a60216ed06e6e1c134222e61eeb57f003a7bd1
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-01-01 16:36:25 +0100

    Add designed buttons from material library instead styles for links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,9 @@ import { MatListModule } from '@angular/material/list';
     RouterLinkActive,
     MatSidenavModule,
     MatListModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'my-spendings';
-}
+export class AppComponent {}

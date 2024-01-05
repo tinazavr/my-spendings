a77884f01860817d23f9fc20dabb5ce3ff38fd74
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-01-05 10:44:59 +0100

    Change order of methods in spendings.

--- apps/my-spendings/src/app/spendings/spendings.component.ts
+++ apps/my-spendings/src/app/spendings/spendings.component.ts
@@ -30,8 +30,8 @@ export class SpendingsComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     await this.loadCategories();
     this.createCategoriesObject();
-    this.setCategoryNames();
     await this.loadSpendings();
+    this.setCategoryNames();
   }
   async loadSpendings(): Promise<void> {
     this.spendingsList = await firstValueFrom(

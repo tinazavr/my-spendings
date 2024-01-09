4ebcaf343a5f85927b69f52f2eac9d59a265efba
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-01-09 23:43:51 +0100

    New category from input  sends to component.

--- apps/my-spendings/src/app/categories/categories.component.ts
+++ apps/my-spendings/src/app/categories/categories.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { Category } from '../interfaces/category';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule, MatListModule],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent implements OnInit {
+
+  addCategoryClicked: boolean = false;
   categoriesList: Category[] = [];
+  newCategoryName: string = '';
 
   loadCategories(): void {
     this.categoriesService.getCategories().subscribe((data) => {
@@ -23,5 +37,30 @@ export class CategoriesComponent implements OnInit {
   ngOnInit(): void {
     this.loadCategories();
   }
+  clickedAddCategory() {
+    this.addCategoryClicked = true;
+    //console.log('add button works!');
+  }
+  submitNewCategory(): void {
+    this.categoriesService.addCategory(this.newCategoryName);
+    console.log('from submitNewCategory method' + this.newCategoryName);
+
+  }
+
+  // changeCategoryName(){
+  //   console.log(this.newCategoryName);
+  // }
+  // getInputValue(event: Event): void {
+  //   console.log(
+  //     'getInputValue method' + (event.target as HTMLInputElement).value
+  //   );
+    //return (event.target as HTMLInputElement).value;
+ // }
+  // submitValue(newTitle: string) {
+  //   this.submit.emit(newTitle);
+  // }
+  // getInputValue(event: Event) {
+  //   return (event.target as HTMLInputElement).value;
+  // }
   constructor(private categoriesService: CategoriesService) {}
 }
--- apps/my-spendings/src/app/services/categories.service.ts
+++ apps/my-spendings/src/app/services/categories.service.ts
@@ -7,12 +7,16 @@ import { Category } from '../interfaces/category';
 })
 export class CategoriesService {
   httpClient: HttpClient = inject(HttpClient);
-  
+
   getCategories() {
     let link = `http://localhost:3020/api/categories`;
     return this.httpClient.get<Category[]>(link);
   }
-
+  addCategory(newCategory: string) {
+    const link = `http://localhost:3020/api/categories`;
+    const body = { categoryName: newCategory };
+    return this.httpClient.post(link, body);
+  }
   constructor() {}
+  
 }
-

ddc4571758b801dc42bb90044e9ce14d05472775
maksymzav <maksym.zavorotynskyi@shopping-feed.com>
2024-01-27 14:52:26 +0100

    Initialize e2e tests

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,11 +1,15 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
-
-import {AppService} from './app.service';
 import {categoriesList, spendingsList} from './stub-data';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
+  }
+
+  @Get()
+  getData() {
+    return this.appService.getData();
   }
 
   @Get('categories')

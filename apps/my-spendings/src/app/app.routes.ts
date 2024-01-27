2e2e3c71fb50f257dfc685e22396730d87e861db
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2024-01-27 19:54:31 +0100

    Create and style component for reset password.

--- apps/my-spendings/src/app/app.routes.ts
+++ apps/my-spendings/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { SpendingsComponent } from './spendings/spendings.component';
 import { LoginComponent } from './login/login.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/spendings', pathMatch: 'full' },
   { path: 'categories', component: CategoriesComponent },
   { path: 'spendings', component: SpendingsComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'password-reset', component: PasswordResetComponent },
 ];
